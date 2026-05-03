192c59d8b1a981145f37caaa9c96c8eb3be0d45a
peervalerian-source <peervalerian-source@users.noreply.github.com>
2026-05-03 15:07:25 +0200

    Deploy virag 2026-05-03 15:07

--- magyarul/assets/useFilterStore-YNqJkto1.js
+++
@@ -1,2 +0,0 @@
-import{e as r}from"./index-UFPx7uHP.js";const e=r(a=>({category:"all",wordClass:"all",difficulty:"all",mastery:"all",setCategory:l=>a({category:l}),setWordClass:l=>a({wordClass:l}),setDifficulty:l=>a({difficulty:l}),setMastery:l=>a({mastery:l}),reset:()=>a({category:"all",wordClass:"all",difficulty:"all",mastery:"all"})}));export{e as u};
-//# sourceMappingURL=useFilterStore-YNqJkto1.js.map
